22f919dec9f5ab6690bee7f8b65e59960cd3894d
jeeyeon <jeeyeon118@gmail.com>
2023-11-21 00:03:18 +0900

    20231121-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Layout from "./components/layout"
 import Home from "./routes/home"
 import Profile from "./routes/profile"
+import CreateAccount from "./routes/create-account"
+import Login from "./routes/login"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 const router = createBrowserRouter([
   {
@@ -17,10 +21,35 @@ const router = createBrowserRouter([
         element: <Profile />,
       }
     ]
+  },
+  // 로그인, 크리에잇은 레이아웃을 벗어난 곳에 배치함. 
+  {
+    path:"/login",
+    element: <Login />,
+  },
+  {
+    path:"/create-account",
+    element: <CreateAccount />,
   }
 ])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box
+  }
+  body{
+    background-color: black
+    color:white
+    font-family: system-ui, -apple-system, BlickMacSystemFont,'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',sans-serif; 
+  }
+`;
+
+
 function App() {
-  return (<>
+  return (
+  <>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </>
   )
